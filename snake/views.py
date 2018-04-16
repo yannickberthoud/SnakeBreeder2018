e0293f251647017e84ebb0ebac1221d99976b2a1
Yannick Berthoud <yannick.berthoud@icloud.com>
2018-04-16 22:32:14 +0200

    Fix

--- snake/views.py
+++ snake/views.py
@@ -45,6 +45,7 @@ class SaleSnakeView(generic.ListView):
 
 def SearchSnakeView(request):
     species_list = get_object_or_404(SearchingSnake, pk=1)
+    
     return render(request, 'snake/search_list.html', {'species_list': species_list})
 
 class DetailView(generic.DetailView):
