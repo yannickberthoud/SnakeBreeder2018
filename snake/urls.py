f137dd063eee25a605020e03a7c8cf886554c0f1
Yannick Berthoud <yannick.berthoud@icloud.com>
2018-04-16 22:24:44 +0200

    Changing urls

--- snake/urls.py
+++ snake/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from snake.views import IndexView, DetailView, OldSnakeView, SaleSnakeView, NewBornView, SearchSnakeView
 
 app_name = 'snake'
+
 urlpatterns = [
     path('a-vendre', SaleSnakeView.as_view(), name = 'sale'),
-    path('recherches', SearchSnakeView.as_view(), name = 'search'),
+    path('recherches', SearchSnakeView, name = 'search'),
     path('anciens-serpents', OldSnakeView.as_view(), name = 'old'),
     path('nouvelles-naissances', NewBornView.as_view(), name = 'newborn'),
     path('<str:family_slug>/<str:slug>', DetailView.as_view(), name = 'details'),
