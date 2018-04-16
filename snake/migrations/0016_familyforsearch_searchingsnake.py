5dc00a7dd9fed6706752d9a9e9ae04a1c3566098
Yannick Berthoud <yannick.berthoud@icloud.com>
2018-04-16 21:48:50 +0200

    Try to fix

--- snake/migrations/0016_familyforsearch_searchingsnake.py
+++ snake/migrations/0016_familyforsearch_searchingsnake.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('snake', '0015_delete_searchingsnake'),
+        
     ]
 
     operations = [
